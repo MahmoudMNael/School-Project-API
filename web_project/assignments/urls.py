e31595d480bafc336becb6d2fd9cab7f17746b8b
Mahmoud Mohammed Nael <mahmoudmnael@gmail.com>
2024-05-23 15:14:45 +0300

    fixed url in assignments module

--- web_project/assignments/urls.py
+++ web_project/assignments/urls.py
@@ -4,7 +4,7 @@ from assignments.views import AssignmentsListView, CommentsListView, Submissions
 
 urlpatterns = [
 	path('', AssignmentsListView.as_view(), name='assignments_list'),
-	path('<int:assignment_id/', AssignmentDetailView.as_view(), name='assignment_detail'),
+	path('<int:assignment_id>/', AssignmentDetailView.as_view(), name='assignment_detail'),
 	path('<int:assignment_id>/comments/', CommentsListView.as_view(), name='comments_list'),
 	# path('<int:assignment_id>/comments/<int:comment_id>/', CommentActionView.as_view(), name='comment_action'),
 	path('<int:assignment_id>/submissions/', SubmissionsListView.as_view(), name='submissions_list'),
